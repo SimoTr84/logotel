56d3325fd3aa1a5bd5dd1603a2d5c5b69ddfc890
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-18 16:13:06 +0200

    feat (views/template): amends to Vue Router and Home template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeroSlider from '../components/HeroSlider.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HeroSlider />
   </main>
 </template>
