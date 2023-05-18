87779c76d6f7ae89bcd97426da315da68fe32cd4
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-18 16:14:08 +0200

    feat (components): created first 2 components, Nav and Hero Slider - WIP
    Work in progress for both Hero Slider and Main Nav

---
+++ src/components/HeroSlider.vue
@@ -0,0 +1,73 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  slideParagraph: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template>
+  <div class="header-banner">
+    <div class="carousel" aria-label="Gallery">
+      <ol class="carousel__viewport">
+        <li id="carousel__slide1" tabindex="0" class="carousel__slide">
+          <div class="title-wrapper">
+            <h1 class="hero-title">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit in id.
+            </h1>
+            <p>
+              Nunc rutrum in arcu eget viverra. Donec aliquet eros id purus gravida, ut convallis leo rutrum. 
+              Aenean non ipsum at ligula vestibulum pharetra. Integer vel scelerisque purus. Donec placerat 
+              lacinia sagittis. Cras gravida tristique mi, tincidunt dapibus neque ultrices finibus.
+            </p>
+          </div>
+          <div class="carousel__snapper">
+            <img src="/src/assets/images/carousel/hills-landscape.webp" alt="Logotel - Hills Landscape Banner Image">
+            <a href="#carousel__slide4" class="carousel__prev">Go to last slide</a>
+            <a href="#carousel__slide2" class="carousel__next">Go to next slide</a>
+          </div>
+        </li>
+        <li id="carousel__slide2" tabindex="0" class="carousel__slide">
+          <div class="carousel__snapper">
+            <img src="/src/assets/images/carousel/mountains-sunset.webp" alt="Logotel - Mountains at Sunset Banner Image">
+          </div>
+          <a href="#carousel__slide1" class="carousel__prev">Go to previous slide</a>
+          <a href="#carousel__slide3" class="carousel__next">Go to next slide</a>
+        </li>
+        <li id="carousel__slide3" tabindex="0" class="carousel__slide">
+          <div class="carousel__snapper">
+            <img src="/src/assets/images/carousel/ocean-waves.webp" alt="Logotel - Ocean Waves Image">
+          </div>
+          <a href="#carousel__slide2" class="carousel__prev">Go to previous slide</a>
+          <a href="#carousel__slide4" class="carousel__next">Go to next slide</a>
+        </li>
+        <li id="carousel__slide4" tabindex="0" class="carousel__slide">
+          <div class="carousel__snapper"></div>
+          <a href="#carousel__slide3" class="carousel__prev">Go to previous slide</a>
+          <a href="#carousel__slide1" class="carousel__next">Go to first slide</a>
+        </li>
+      </ol>
+      <aside class="carousel__navigation">
+        <ol class="carousel__navigation-list">
+          <li class="carousel__navigation-item">
+            <a href="#carousel__slide1" class="carousel__navigation-button">Go to slide 1</a>
+          </li>
+          <li class="carousel__navigation-item">
+            <a href="#carousel__slide2" class="carousel__navigation-button">Go to slide 2</a>
+          </li>
+          <li class="carousel__navigation-item">
+            <a href="#carousel__slide3" class="carousel__navigation-button">Go to slide 3</a>
+          </li>
+          <li class="carousel__navigation-item">
+            <a href="#carousel__slide4" class="carousel__navigation-button">Go to slide 4</a>
+          </li>
+        </ol>
+      </aside>
+    </div>
+  </div>
+</template>
+
---
+++ src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="nav">
+    <img src="" alt="">
+    <div class="menu-wrapper">
+        <ul class="menu">
+          <li class="menu-item">Slider</li>
+          <li class="menu-item">News</li>
+          <li class="menu-item">Percorso</li>
+          <li class="menu-item">Tab</li>
+        </ul>
+    </div>
+  </div>
+</template>
