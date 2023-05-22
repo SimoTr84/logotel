d20aba362def31e4fc84c883d43bb1c77ef88b91
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-22 12:56:20 +0200

    feat (component): added hover cards component + styles

---
+++ src/components/HoverCards.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hover-cards__container">
+    <div class="hover-card">
+      <div class="hover-card__content">
+        <h5 class="hover-card__title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <div class="hover-card__divider"></div>
+        <img src="src/assets/images/icons/icona-sveglia.svg" alt="Logotel - Icona Sveglia">
+        <p class="hover-card__paragraph">
+          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+          aliquam erat volutpat. Ut wisi enim ad minim…
+        </p>
+        <a class="cta"></a>
+      </div>
+    </div>
+    <div class="hover-card">
+      <img src="src/assets/images/portraits/card-portrait-1.jpg" alt="Logotel - Woman's Portrait">
+      <div class="hover-card__content">
+        <h5 class="hover-card__title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="hover-card__paragraph">
+          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+          aliquam erat volutpat. Ut wisi enim ad minim…
+        </p>
+      </div>
+    </div>
+    <div class="hover-card">
+      <img src="src/assets/images/portraits/card-portrait-1.jpg" alt="Logotel - Woman's Portrait">
+      <div class="hover-card__content">
+        <h5 class="hover-card__title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="hover-card__paragraph">
+          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+          aliquam erat volutpat. Ut wisi enim ad minim…
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
