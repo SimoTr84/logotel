bd9a9c1b275cddf39c6d1249562f791a105e7b4a
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-22 12:58:05 +0200

    feat (stylesheets): updated styles to include new components + globals and variables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
+import Navigation from '../components/Navigation.vue'
 import HeroSlider from '../components/HeroSlider.vue'
+import FlipCards from '../components/FlipCards.vue'
+import SectionTitle from '../components/SectionTitle.vue'
+import HoverCards from '../components/HoverCards.vue'
 </script>
 
 <template>
   <main>
+    <Navigation />
     <HeroSlider />
+    <div class="container">
+      <FlipCards />
+    </div>
+    <SectionTitle />
+    <div class="container">
+      <HoverCards />
+    </div>
   </main>
 </template>
+
+
