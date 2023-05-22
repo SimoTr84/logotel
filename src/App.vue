384a33ee3d576a593dbf39774ea8ec2fe33de4a3
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-22 15:15:23 +0200

    chore (cleanup): minor tidy up of hero slider styles and main scss file + moved navigation from app file to home template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HeroSlider from './components/HeroSlider.vue'
-import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
-  <Navigation />
-
   <RouterView />
 </template>
