86ab7becd942f11584a77b320625bf8d93a84657
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-22 12:56:02 +0200

    feat (component): added flip cards component + styles

---
+++ src/components/FlipCards.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="flip-cards__container">
+    <div class="flip-card">
+      <div class="flip-card__overlay">
+        <p class="flip-card__overlay-date">
+          13.09.2020
+        </p>
+        <h5 class="flip-card__overlay-title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="flip-card__overlay-paragraph">
+          Quis aute iure reprehenderit in voluptate velit esse cillum dolore 
+          eu fugiat nulla pariatur. Excepteur sint obcaecat cupidi aute iure 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa 
+          qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <img src="src/assets/images/portraits/card-portrait-1.jpg" alt="Logotel - Woman's Portrait">
+      <div class="flip-card__content">
+        <p class="flip-card__date">
+          13.09.2020
+        </p>
+        <h5 class="flip-card__title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="flip-card__paragraph">
+          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+          aliquam erat volutpat. Ut wisi enim ad minim…
+        </p>
+      </div>
+    </div>
+    <div class="flip-card">
+      <div class="flip-card__overlay">
+        <p class="flip-card__overlay-date">
+          13.09.2020
+        </p>
+        <h5 class="flip-card__overlay-title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="flip-card__overlay-paragraph">
+          Quis aute iure reprehenderit in voluptate velit esse cillum dolore 
+          eu fugiat nulla pariatur. Excepteur sint obcaecat cupidi aute iure 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa 
+          qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <img src="src/assets/images/portraits/card-portrait-2.jpg" alt="Logotel - Woman's Portrait">
+      <div class="flip-card__content">
+        <p class="flip-card__date">
+          13.09.2020
+        </p>
+        <h5 class="flip-card__title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="flip-card__paragraph">
+          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+          aliquam erat volutpat. Ut wisi enim ad minim…
+        </p>
+      </div>
+    </div>
+    <div class="flip-card">
+      <div class="flip-card__overlay">
+        <p class="flip-card__overlay-date">
+          13.09.2020
+        </p>
+        <h5 class="flip-card__overlay-title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="flip-card__overlay-paragraph">
+          Quis aute iure reprehenderit in voluptate velit esse cillum dolore 
+          eu fugiat nulla pariatur. Excepteur sint obcaecat cupidi aute iure 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa 
+          qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <img src="src/assets/images/portraits/card-portrait-3.jpg" alt="Logotel - Man's Portrait">
+      <div class="flip-card__content">
+        <p class="flip-card__date">
+          13.09.2020
+        </p>
+        <h5 class="flip-card__title">
+          Lorem Ipsum dolor sit amet, consectetuer
+        </h5>
+        <p class="flip-card__paragraph">
+          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+          aliquam erat volutpat. Ut wisi enim ad minim…
+        </p>
+      </div>
+    </div>
+    <!-- <figure class="flip-card">
+      <img src="src/assets/images/portraits/card-portrait-2.jpg" alt="Logotel - Woman's Portrait">
+      <figcaption>The Night</figcaption>
+    </figure>
+    <figure class="flip-card">
+      <img src="src/assets/images/portraits/card-portrait-3.jpg" alt="Logotel - Man's Portrait">
+      <figcaption>The Night</figcaption>
+    </figure> -->
+  </div>
+</template>
