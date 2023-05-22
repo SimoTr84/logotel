0b0ebe8a8171d8fa245010c2b88eca0be62cf1bc
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-22 12:57:04 +0200

    feat (component): added main component + styles

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="nav">
-    <img src="" alt="">
+    <img src="/src/assets/images/logos/logo-bianco.svg" class="logo" alt="Logotel Logo Ufficiale">
     <div class="menu-wrapper">
         <ul class="menu">
-          <li class="menu-item">Slider</li>
-          <li class="menu-item">News</li>
-          <li class="menu-item">Percorso</li>
-          <li class="menu-item">Tab</li>
+          <li class="menu-item">
+            <a href="#">Slider</a>
+          </li>
+          <li class="menu-item">
+            <a href="#">News</a>
+          </li>
+          <li class="menu-item">
+            <a href="#">Percorso</a>
+          </li>
+          <li class="menu-item">
+            <a href="#">Tab</a>
+          </li>
         </ul>
     </div>
   </div>
