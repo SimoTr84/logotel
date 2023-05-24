418b6ef852456ca3dd267e84b125c6c41992a157
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-24 22:19:39 +0200

    feat (component): updated hover cards component - template

--- src/components/HoverCards.vue
+++ src/components/HoverCards.vue
@@ -1,57 +1,62 @@
+<script setup>
+import TitleSectionLeft from '../components/TitleSectionLeft.vue'
+
+</script>
 <template>
-<div class="hover-cards__wrapper">
-    <div class="hover-cards__container container">
-      <div class="hover-card pink">
-        <div class="hover-card__content">
-          <h5 class="hover-card__title">
-            Tab Rosa
-          </h5>
-          <div class="hover-card__divider"></div>
-          <div class="hover-card__img-wrapper">
-            <div class="hover-card__icon-bg"></div>
-            <img src="src/assets/images/icons/icona-sveglia.svg" alt="Logotel - Icona Sveglia">
+  <TitleSectionLeft />
+  <div class="hover-cards__wrapper">
+      <div class="hover-cards__container container">
+        <div class="hover-card pink">
+          <div class="hover-card__content">
+            <h5 class="hover-card__title">
+              Tab Rosa
+            </h5>
+            <div class="hover-card__divider"></div>
+            <div class="hover-card__img-wrapper">
+              <div class="hover-card__icon-bg"></div>
+              <img src="src/assets/images/icons/icona-sveglia.svg" alt="Logotel - Icona Sveglia">
+            </div>
+            <p class="hover-card__paragraph">
+              Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+              aliquam erat volutpat. Ut wisi enim ad minim…
+            </p>
+            <a class="btn hover-card__btn">Call to action</a>
           </div>
-          <p class="hover-card__paragraph">
-            Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-            aliquam erat volutpat. Ut wisi enim ad minim…
-          </p>
-          <a class="btn hover-card__btn">Call to action</a>
+        </div>
+        <div class="hover-card yellow">
+          <div class="hover-card__content">
+            <h5 class="hover-card__title">
+              Tab Gialla
+            </h5>
+            <div class="hover-card__divider"></div>
+            <div class="hover-card__img-wrapper">
+              <div class="hover-card__icon-bg"></div>
+              <img src="src/assets/images/icons/icona-target.svg" alt="Logotel - Icona Target">
+            </div>
+            <p class="hover-card__paragraph">
+              Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+              aliquam erat volutpat. Ut wisi enim ad minim…
+            </p>
+            <a class="btn hover-card__btn">Call to action</a>
+          </div>
+        </div>
+        <div class="hover-card teal">
+          <div class="hover-card__content">
+            <h5 class="hover-card__title">
+              Tab Turchese
+            </h5>
+            <div class="hover-card__divider"></div>
+            <div class="hover-card__img-wrapper">
+              <div class="hover-card__icon-bg"></div>
+              <img src="src/assets/images/icons/icona-razzo.svg" alt="Logotel - Icona Razzo">
+            </div>
+            <p class="hover-card__paragraph">
+              Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
+              aliquam erat volutpat. Ut wisi enim ad minim…
+            </p>
+            <a class="btn hover-card__btn">Call to action</a>
+          </div>
         </div>
       </div>
-      <div class="hover-card yellow">
-        <div class="hover-card__content">
-          <h5 class="hover-card__title">
-            Tab Gialla
-          </h5>
-          <div class="hover-card__divider"></div>
-          <div class="hover-card__img-wrapper">
-            <div class="hover-card__icon-bg"></div>
-            <img src="src/assets/images/icons/icona-target.svg" alt="Logotel - Icona Target">
-          </div>
-          <p class="hover-card__paragraph">
-            Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-            aliquam erat volutpat. Ut wisi enim ad minim…
-          </p>
-          <a class="btn hover-card__btn">Call to action</a>
-        </div>
-      </div>
-      <div class="hover-card teal">
-        <div class="hover-card__content">
-          <h5 class="hover-card__title">
-            Tab Turchese
-          </h5>
-          <div class="hover-card__divider"></div>
-          <div class="hover-card__img-wrapper">
-            <div class="hover-card__icon-bg"></div>
-            <img src="src/assets/images/icons/icona-razzo.svg" alt="Logotel - Icona Razzo">
-          </div>
-          <p class="hover-card__paragraph">
-            Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-            aliquam erat volutpat. Ut wisi enim ad minim…
-          </p>
-          <a class="btn hover-card__btn">Call to action</a>
-        </div>
-      </div>
-    </div>
-</div>
+  </div>
 </template>
