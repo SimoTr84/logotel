c690acb753b0a0a7fc465fd2d67d97deebf746af
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-24 22:14:04 +0200

    feat (component): added title section/component

---
+++ src/components/TitleSectionLeft.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="title-section title-section__left">
+    <div class="container">
+      <div class="title-section__wrapper">
+        <h1>
+          Lorem ipsum dolor <br> 
+          sit amet consectetuer?
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/TitleSectionRight.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="title-section title-section__right">
+    <div class="container">
+      <div class="title-section__wrapper">
+        <h1>
+          Sed diam nonummy nibh<br>
+          euismod tincidunt?
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
