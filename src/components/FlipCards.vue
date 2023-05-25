d214eec0f96984652201937a4c82bfac294bf60f
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-25 15:07:34 +0200

    feat (component): added dynamic data for flip cards component

--- src/components/FlipCards.vue
+++ src/components/FlipCards.vue
@@ -1,102 +1,60 @@
 <template>
-  <div class="flip-cards__container">
-    <div class="flip-card">
-      <div class="flip-card__overlay">
-        <p class="flip-card__overlay-date">
-          13.09.2020
-        </p>
-        <h5 class="flip-card__overlay-title">
-          Lorem Ipsum dolor sit amet, consectetuer
-        </h5>
-        <p class="flip-card__overlay-paragraph">
-          Quis aute iure reprehenderit in voluptate velit esse cillum dolore 
-          eu fugiat nulla pariatur. Excepteur sint obcaecat cupidi aute iure 
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa 
-          qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <img src="src/assets/images/portraits/card-portrait-1.jpg" alt="Logotel - Woman's Portrait">
-      <div class="flip-card__content">
-        <p class="flip-card__date">
-          13.09.2020
-        </p>
-        <h5 class="flip-card__title">
-          Lorem Ipsum dolor sit amet, consectetuer
-        </h5>
-        <p class="flip-card__paragraph">
-          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-          aliquam erat volutpat. Ut wisi enim ad minim…
-        </p>
+  <div class="news" id="news">
+    <div class="flip-cards__container">
+      <div class="flip-card" v-for="article in articles" :key="article.id">
+        <div class="flip-card__overlay">
+          <p class="flip-card__overlay-date">
+            {{ article.date }}
+          </p>
+          <h5 class="flip-card__overlay-title">
+            {{ article.titleOverlay }}
+          </h5>
+          <p class="flip-card__overlay-paragraph">
+            {{ article.body }}
+          </p>
+        </div>
+        <img :src="`src/assets/images/portraits/${article.image}`" :alt="`${article.imageAlt}`">
+        <div class="flip-card__content">
+          <p class="flip-card__date">
+            {{ article.date }}
+          </p>
+          <h5 class="flip-card__title">
+            {{ article.title }}
+          </h5>
+          <p class="flip-card__paragraph">
+            {{ article.excerpt }}
+          </p>
+        </div>
       </div>
     </div>
-    <div class="flip-card">
-      <div class="flip-card__overlay">
-        <p class="flip-card__overlay-date">
-          13.09.2020
-        </p>
-        <h5 class="flip-card__overlay-title">
-          Lorem Ipsum dolor sit amet, consectetuer
-        </h5>
-        <p class="flip-card__overlay-paragraph">
-          Quis aute iure reprehenderit in voluptate velit esse cillum dolore 
-          eu fugiat nulla pariatur. Excepteur sint obcaecat cupidi aute iure 
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa 
-          qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <img src="src/assets/images/portraits/card-portrait-2.jpg" alt="Logotel - Woman's Portrait">
-      <div class="flip-card__content">
-        <p class="flip-card__date">
-          13.09.2020
-        </p>
-        <h5 class="flip-card__title">
-          Lorem Ipsum dolor sit amet, consectetuer
-        </h5>
-        <p class="flip-card__paragraph">
-          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-          aliquam erat volutpat. Ut wisi enim ad minim…
-        </p>
-      </div>
-    </div>
-    <div class="flip-card">
-      <div class="flip-card__overlay">
-        <p class="flip-card__overlay-date">
-          13.09.2020
-        </p>
-        <h5 class="flip-card__overlay-title">
-          Lorem Ipsum dolor sit amet, consectetuer
-        </h5>
-        <p class="flip-card__overlay-paragraph">
-          Quis aute iure reprehenderit in voluptate velit esse cillum dolore 
-          eu fugiat nulla pariatur. Excepteur sint obcaecat cupidi aute iure 
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa 
-          qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <img src="src/assets/images/portraits/card-portrait-3.jpg" alt="Logotel - Man's Portrait">
-      <div class="flip-card__content">
-        <p class="flip-card__date">
-          13.09.2020
-        </p>
-        <h5 class="flip-card__title">
-          Lorem Ipsum dolor sit amet, consectetuer
-        </h5>
-        <p class="flip-card__paragraph">
-          Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-          aliquam erat volutpat. Ut wisi enim ad minim…
-        </p>
-      </div>
-    </div>
-    <!-- <figure class="flip-card">
-      <img src="src/assets/images/portraits/card-portrait-2.jpg" alt="Logotel - Woman's Portrait">
-      <figcaption>The Night</figcaption>
-    </figure>
-    <figure class="flip-card">
-      <img src="src/assets/images/portraits/card-portrait-3.jpg" alt="Logotel - Man's Portrait">
-      <figcaption>The Night</figcaption>
-    </figure> -->
   </div>
 </template>
+
+<script>
+import articleData from '../assets/data/news.json'
+
+export default {
+  data() {
+    return {
+      articles: articleData
+    };
+  },
+
+  methods: {
+    cutArticleCopy(article)  {
+      // this function is working behind the scenes but I wasn't able to inject the value of the 'text' variable into the template.
+      let paragraphs = document.getElementsByClassName("flip-card__paragraph");
+      for (let paragraph of paragraphs) {
+        let text = paragraph.innerHTML;
+        if (text.length > 75 ) {
+          text = text.substring(0, 75) + "...";
+        }
+      }
+    }
+  },
+
+  mounted() {
+    this.cutArticleCopy();
+  }
+};
+</script>
