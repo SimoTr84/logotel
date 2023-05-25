94f14030a1bf56d6529349b03baabdafa3803a0b
Simone Trucco <simone.trucco@orchard.com.au>
2023-05-25 15:06:52 +0200

    fix (component): reworked navigation - added mobile nav + nav functions

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="nav">
-    <img src="/src/assets/images/logos/logo-bianco.svg" class="logo" alt="Logotel Logo Ufficiale">
-    <div class="menu-wrapper">
-        <ul class="menu">
-          <li class="menu-item">
-            <a href="#">Slider</a>
-          </li>
-          <li class="menu-item">
-            <a href="#">News</a>
-          </li>
-          <li class="menu-item">
-            <a href="#">Percorso</a>
-          </li>
-          <li class="menu-item">
-            <a href="#">Tab</a>
-          </li>
-        </ul>
+  <section class="top-nav" id="top-nav">
+    <div class="nav__logo">
+      <img src="/src/assets/images/logos/logo-bianco.svg" class="nav__logo" alt="Logotel Logo Ufficiale">
     </div>
-  </div>
+    <input id="menu__toggle" type="checkbox" />
+    <label class='menu__button-container' for="menu__toggle">
+      <div class='menu__button'></div>
+    </label>
+    <ul class="menu">
+      <li>
+        <a href="#" class="js-nav-item" data-target="slider">Slider</a>
+      </li>
+      <li>
+        <a href="#" class="js-nav-item" data-target="news">News</a>
+      </li>
+      <li>
+        <a href="#" class="js-nav-item" data-target="percorso">Percorso</a>
+      </li>
+      <li>
+        <a href="#" class="js-nav-item" data-target="tab">Tab</a>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script>
+export default {
+  methods: {
+    // scrollToSection(event) {
+    //   let navItem = event.target;
+    //   let navHeight = document.getElementById("nav").clientHeight;
+    //   let targetURL = event.target.href;
+    //   console.log(1, event.target);
+    //   console.log(2, navHeight);
+    //   console.log(3, targetURL);
+
+
+    //   navItem.addEventListener("click", function() {
+    //     window.scrollTo({
+    //       behavior: "smooth",
+    //       top: event.target - navHeight
+    //     })
+    //     console.log(4, navItem);
+    //     console.log(5, navHeight);
+    //   })
+
+    // },
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import './assets/scss/main.scss'
+import './assets/styles/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -13,3 +13,39 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// Custom scripts added here
+
+// HANDLE THE SCOLL ANIMATION FOR MAIN NAVIGATION
+window.onscroll = function () {
+  let navBar = document.getElementById('top-nav');
+  console.log(navBar);
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    navBar.classList.add('sticky');
+  } else {
+    navBar.classList.remove('sticky');
+  }
+};
+
+document.addEventListener('click', function (event) {
+  if (!event.target.matches('.js-nav-item')) return;
+  event.preventDefault();
+
+  let element = document.getElementById(event.target.dataset.target);
+  let navHeight = document.getElementById('top-nav').clientHeight;
+  let elementPosition = element.getBoundingClientRect().top;
+  let offsetPosition = elementPosition + window.pageYOffset - navHeight - 20;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: 'smooth'
+  })
+  
+})
+
+// HANDLE THE SLIDER NAVIGATION BUTTONS
+// add datasets to each slide item and on click of either prev or next, read the target slide
+// once target slide is defined then animate the corresponding button number
+
+
+//
